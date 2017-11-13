1d7b51976335ac5eb87c371e6bbe4a4b039bdd22
Gajus Kuizinas <gajus@gajus.com>
2017-11-13 14:15:10 +0000

    test: fix type path

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -2,7 +2,7 @@
 
 import type {
   ColumnType
-} from '../src/types';
+} from '../../src/types';
 
 export const createColumnWithName = (columnName: string): ColumnType => {
   // eslint-disable-next-line no-extra-parens
