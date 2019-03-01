97d1593ba0c77c096f82fbc742482ebd4b1826d1
Gajus Kuizinas <gajus@gajus.com>
2019-03-01 11:38:26 +0000

    test: creates a loader for _id columns

--- test/postloader/utilities/generateDataLoaderFactory.js
+++ test/postloader/utilities/generateDataLoaderFactory.js
@@ -6,7 +6,7 @@ import {
 } from 'lodash';
 import generateDataLoaderFactory from '../../../src/utilities/generateDataLoaderFactory';
 
-test('creates a single loader', (t) => {
+test('creates a loader for unique indexes', (t) => {
   const actual = trim(generateDataLoaderFactory(
     [
       {
@@ -63,6 +63,61 @@ export const createLoaders = (connection: DatabaseConnectionType, NotFoundError:
 
   return {
     BazByBarLoader
+  };
+};`);
+
+  // eslint-disable-next-line ava/prefer-power-assert
+  t.is(actual, expected);
+});
+
+test('creates a loader for _id columns', (t) => {
+  const actual = trim(generateDataLoaderFactory(
+    [
+      {
+        comment: '',
+        dataType: 'text',
+        isMaterializedView: false,
+        isNullable: false,
+        mappedTableName: 'baz',
+        name: 'bar_id',
+        tableName: 'foo'
+      }
+    ],
+    [],
+    {}
+  ));
+
+  const expected = trim(`
+// @flow
+
+import {
+  getByIds,
+  getByIdsUsingJoiningTable
+} from 'postloader';
+import DataLoader from 'dataloader';
+import type {
+  DatabaseConnectionType
+} from 'slonik';
+
+type BazRecordType = {|
+  +barId: string
+|};
+
+export type {
+  BazRecordType
+};
+
+export type LoadersType = {|
+  +BazsByBarIdLoader: DataLoader<string, $ReadOnlyArray<BazRecordType>>
+|};
+
+export const createLoaders = (connection: DatabaseConnectionType, NotFoundError: Class<Error>): LoadersType => {
+  const BazsByBarIdLoader = new DataLoader((ids) => {
+    return getByIds(connection, 'foo', ids, 'bar_id', '"bar_id" "barId"', true, NotFoundError);
+  });
+
+  return {
+    BazsByBarIdLoader
   };
 };`);
 
