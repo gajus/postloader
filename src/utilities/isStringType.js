e387aaac2098ece651e31fe986797ecdca53586d
Gajus Kuizinas <gajus@gajus.com>
2019-07-03 12:48:01 +0100

    fix: add UUID type

--- src/utilities/isStringType.js
+++ src/utilities/isStringType.js
@@ -3,7 +3,7 @@
 /**
  * @see https://www.postgresql.org/docs/current/static/datatype-numeric.html
  */
-const characterTypeRule = /^(?:text|character|coordinates)(\s|$)/;
+const characterTypeRule = /^(?:text|character|coordinates|uuid)(\s|$)/;
 
 export default (databaseTypeName: string): boolean => {
   return characterTypeRule.test(databaseTypeName);
